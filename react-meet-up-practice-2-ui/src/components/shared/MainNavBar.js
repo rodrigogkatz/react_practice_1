4bc5c5ee5e5ab8b124dc5df727394e9fd075a94e
rodrigo <rodrigog.katz@hotmail.com>
2022-07-13 15:40:54 -0300

    added styles to  menu

--- react-meet-up-practice-2-ui/src/components/shared/MainNavBar.js
+++ react-meet-up-practice-2-ui/src/components/shared/MainNavBar.js
@@ -1,3 +1,4 @@
+import classes from './MainNavBar.module.css';
 import { Link } from 'react-router-dom';
 // the Link is like <a href=''>
 // react makes it render an anchor tag and internaly attaches an onClick listener
@@ -5,13 +6,13 @@ import { Link } from 'react-router-dom';
 // changes it in the browser url bar, but then loads the appropriate component into the screen
 
 function MainNavBar(props){
-  return <header>
-    <div className='header-logo'></div>
-    <nav>
-      <ul>
-        <li><Link to='/'>All Meetups</Link></li>
-        <li><Link to='/new-meetup'>New Meetups</Link></li>
-        <li><Link to='/favorites'>Favorites</Link></li>
+  return <header className={classes.header}>
+    <div className={classes.header_logo}>Meetups App</div>
+    <nav >
+      <ul >
+        <li><Link to='/' >All Meetups</Link></li>
+        <li><Link to='/new-meetup' >New Meetups</Link></li>
+        <li><Link to='/favorites' >Favorites</Link></li>
       </ul>
     </nav>
     <div></div>
