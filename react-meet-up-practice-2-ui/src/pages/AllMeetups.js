0c7f4b4b59ba07d637582774736647757f7f890f
rodrigo <rodrigog.katz@hotmail.com>
2022-08-29 15:11:54 -0300

    no me acuerdo que hice aca

--- react-meet-up-practice-2-ui/src/pages/AllMeetups.js
+++ react-meet-up-practice-2-ui/src/pages/AllMeetups.js
@@ -1,5 +1,33 @@
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  }
+];
+
 function AllMeetupsPage(){
-  return <div>All Meetup page working!</div>;
+  return <div>
+    <h1>All Meetups</h1>
+    <ul>
+      {DUMMY_MEETUPS.map((meetup) => {
+        return <li key={meetup.id}>{meetup.title}</li>
+      })}
+    </ul>
+  </div>;
 }
 
 export default AllMeetupsPage;
